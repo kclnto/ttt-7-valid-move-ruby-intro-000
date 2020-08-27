adac28650756108d4ecf8883e4062ad508f1497a
Kacy Trujillo <kclnto@gmail.com>
2020-08-27 17:55:08 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,13 @@
 def valid_move?(board, index)
   if index.between?(0, 8)
     TRUE
+    if position_taken?(board, index) == TRUE
+      FALSE
+    else
+      TRUE
+    end
   else
     FALSE
-  end
-  if position_taken?(board, index) == TRUE
-    FALSE
-  else
-    TRUE
   end
 end
 
